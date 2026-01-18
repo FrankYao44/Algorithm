f768055890713f4c4e950099f3fe320742a063f3
Your Name <you@example.com>
2026-01-18 09:04:48 +0000

    change the proxy file. Meanwhlie add some useful network tool to scripts.

--- .devcontainer/container_prerun.sh
+++ .devcontainer/container_prerun.sh
@@ -7,3 +7,11 @@ sudo chmod 600 /home/coder/.ssh/*
 sudo rm /home/coder/.ssh/known_hosts
 sudo rm /home/coder/.ssh/known_hosts.old
 ssh -o StrictHostKeyChecking=accept-new  -T git@github.com 
+# 设置 git 代理与容器环境变量一致
+if [ -n "$http_proxy" ]; then
+    git config --global http.proxy "$http_proxy"
+    git config --global https.proxy "$https_proxy"
+    echo "Git proxy set to $http_proxy"
+else
+    echo "http_proxy not set, skipping git proxy configuration"
+fi
