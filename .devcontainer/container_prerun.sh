896a8f500af598f486ec74c2b75228a91c140ed8
frankyao44 <fy44.zxcs@gmail.com>
2026-03-24 11:15:59 +0000

    放弃git自动拷贝密钥，目前prerun只执行adduer等命令

--- .devcontainer/container_prerun.sh
+++ .devcontainer/container_prerun.sh
@@ -1,15 +1,14 @@
-mkdir -p /home/coder/.ssh
-sudo cp /home/coder/.ssh_host/* /home/coder/.ssh
-sudo chown -R coder:coder /workspace
-sudo chown -R coder:coder /home/coder/.ssh
-sudo chmod 700 /home/coder/.ssh 
-sudo chmod 600 /home/coder/.ssh/*
-sudo rm /home/coder/.ssh/known_hosts
-sudo rm /home/coder/.ssh/known_hosts.old
+# mkdir -p /home/coder/.ssh
+# sudo cp /home/coder/.ssh_host/* /home/coder/.ssh
+# sudo chown -R coder:coder /workspace
+# sudo chown -R coder:coder /home/coder/.ssh
+# sudo chmod 700 /home/coder/.ssh 
+# sudo chmod 600 /home/coder/.ssh/*
+# sudo rm /home/coder/.ssh/known_hosts
+# sudo rm /home/coder/.ssh/known_hosts.old
 
-git config --global user.email "fy44.zxcs@gmail.com"
-git config --global user.name "frankyao44"
-ssh -o StrictHostKeyChecking=accept-new  -T git@github.com 
+
+# ssh -o StrictHostKeyChecking=accept-new  -T git@github.com 
 # 设置 git 代理与容器环境变量一致
 # if [ -n "$http_proxy" ]; then
 #     git config --global http.proxy "$http_proxy"
@@ -18,3 +17,6 @@ ssh -o StrictHostKeyChecking=accept-new  -T git@github.com
 # else
 #     echo "http_proxy not set, skipping git proxy configuration"
 # fi
+
+
+git config --global user.email "fy44.zxcs@gmail.com" && git config --global user.name "frankyao44"
